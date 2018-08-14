0d031892f427c0a774ae4640fba92fc1a617549c
Kemenyfy <gustavo.kemenyfy@gmail.com>
2018-08-14 07:48:51 -0400

    Sorted Movies by Release Dates

--- src/MoviesList.js
+++ src/MoviesList.js
@@ -5,11 +5,23 @@ import movies from './movies.json'
 class MoviesList extends Component {
   render() {
     return (
-      <section className="MoviesList">
-        <ul>
-          {movies.results.map((movie, idx) => {
-            return <Movie name={movie.title} image={movie.poster_path} overview={movie.overview} popularity={movie.popularity} release_date={movie.release_date} key={idx} />
-          })}
+      <section>
+        <ul className="MoviesList"> {movies.results.sort((a, b) => {
+          if (a.release_date > b.release_date) {
+            return 1
+          }
+          else if (a.release_date < b.release_date) {
+            return -1
+          }
+          else {
+            return 0
+          }}).map((movie, idx) => {
+            return <Movie name={movie.title} 
+                    image={movie.poster_path}
+                    overview={movie.overview}
+                    popularity={movie.popularity}
+                    release_date={movie.release_date} key={idx} />
+        })}
         </ul>
       </section>
     )
